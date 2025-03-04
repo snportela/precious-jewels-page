deb3a7efaac183eb60d6f6353e67354c65d0779f
Sophia Portela <sophianobreportela@gmail.com>
2025-03-04 15:45:34 -0300

    fixes on indentation

--- js/nav_btn.js
+++ js/nav_btn.js
@@ -1,13 +1,8 @@
-
-
 function expand() {
-    let x = document.querySelector(".nav-container");
-    if(x.className === "nav-container") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav-container"
-    }
+  let x = document.querySelector(".nav-container");
+  if(x.className === "nav-container") {
+      x.className += " responsive";
+  } else {
+      x.className = "nav-container"
+  }
 }
-
-
-
